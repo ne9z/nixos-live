8560656a56c4c2ca454e003897709f6c9685463a
Maurice Zhou <yuchen@apvc.uk>
2023-04-06 21:21:09 +0200

    prepare script for ci/cd
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- qemu.sh
+++ qemu.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 rm -f disk1.img disk2.img
 
  qemu-img create -f raw disk1.img 16G
  qemu-img create -f raw disk2.img 16G
 
 time qemu-system-x86_64 -enable-kvm \
- -cdrom result/iso/*.iso \
+ -cdrom $(find -type f -name '*.iso') \
  -drive format=raw,file=disk1.img \
  -drive format=raw,file=disk2.img \
  -nographic \
  -monitor none \
  -serial stdio \
- -m 2G \
+ -m 1G \
  -object rng-random,id=id,filename=/dev/random
