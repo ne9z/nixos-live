4ff9871b4d4fa3fe9a32ce8102cef3c4fc77ec7c
Maurice Zhou <yuchen@apvc.uk>
2023-04-07 09:46:57 +0200

    update git clone commands
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- init.sh
+++ init.sh
@@ -4,13 +4,12 @@ sleep 20
 
 nix-env -f '<nixpkgs>' -iA git
 
-git clone https://github.com/ne9z/nixos-live
-git -C ./nixos-live checkout dev
-git clone --depth 1 https://github.com/ne9z/openzfs-docs
-git -C ./openzfs-docs checkout dev
+git clone --branch dev https://github.com/ne9z/nixos-live
+git clone --depth 1 --branch dev https://github.com/ne9z/openzfs-docs
 
 sed 's|.. ifconfig:: zfs_root_test|::|g' \
     'openzfs-docs/docs/Getting Started/NixOS/Root on ZFS.rst' > nixos.rst
+
 nix develop ./nixos-live#build-script --command bash <<EOF
 python \
     ./nixos-live/my_pylit.py \
