b67ffc9b6f1816f2fbe376fc8dec681a7ca07bf1
Maurice Zhou <yuchen@apvc.uk>
2023-04-09 10:08:56 +0200

    shellcheck
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- init.sh
+++ init.sh
@@ -1,27 +1,45 @@
 #!/usr/bin/env bash
 # wait for networking to complete initialization
-set -fxue
+set -vfxue
 
-if [ $(tty) == "/dev/ttyS0" ]; then
+if [ "$(tty || true)" = "/dev/ttyS0" ]; then
 
 sleep 16
 
 git clone --branch main https://github.com/ne9z/nixos-live
 git clone --depth 1 --branch master https://github.com/ne9z/openzfs-docs
 
-sed 's|.. ifconfig:: zfs_root_test|::|g' \
-    'openzfs-docs/docs/Getting Started/NixOS/Root on ZFS.rst' > nixos.rst
+nix develop ./nixos-live#build-script --command bash <<-'EOF'
+run_test () {
+    local path="${1}"
+    local distro="${2}"
+    sed 's|.. ifconfig:: zfs_root_test|::|g' \
+	"${path}" > "${distro}".rst
 
-nix develop ./nixos-live#build-script --command bash <<EOF
-python \
-    ./nixos-live/zfs_root_gen_bash.py \
-    "nixos.rst" \
-    "nixos.sh"
+    # Generate installation script from documentation
+    python scripts/zfs_root_gen_bash.py "${distro}".rst "${distro}".sh
+
+    # Postprocess script for bash
+    sed -i 's|^ *::||g' "${distro}".sh
+    # ensure heredocs work
+    sed -i 's|^ *ZFS_ROOT_GUIDE_TEST|ZFS_ROOT_GUIDE_TEST|g' "${distro}".sh
+    sed -i 's|^ *ZFS_ROOT_NESTED_CHROOT|ZFS_ROOT_NESTED_CHROOT|g' "${distro}".sh
+
+    # check whether nixos.sh have syntax errors
+    bash -n "${distro}".sh
+    shellcheck \
+        --check-sourced \
+        --enable=all \
+        --shell=dash \
+        --severity=style \
+        --format=tty \
+        "${distro}".sh
+
+    # Make the installation script executable and run
+    chmod a+x "${distro}".sh
+}
+cd openzfs-docs
+run_test 'docs/Getting Started/NixOS/Root on ZFS.rst' nixos
 EOF
-
-# check whether nixos.sh have syntax errors
-bash -n nixos.sh
-
-chmod a+x nixos.sh
-./nixos.sh
+./openzfs-docs/nixos.sh nixos
 fi
--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,8 @@
 
 set -fxue
 
-if [ "$1" == "reboot" ]; then
-      time qemu-system-x86_64 \
+if [ "$1" = "reboot" ]; then
+      qemu-system-x86_64 \
  -enable-kvm \
  -drive format=raw,file=disk1.img \
  -drive format=raw,file=disk2.img \
@@ -11,17 +11,17 @@ if [ "$1" == "reboot" ]; then
  -object rng-random,id=id,filename=/dev/random
 fi
 
-if [ "$1" == "rm" ]; then
-time nix build .#nixosConfigurations.exampleHost.config.system.build.isoImage
+if [ "$1" = "rm" ]; then
+nix build .#nixosConfigurations.exampleHost.config.system.build.isoImage
 
 rm -f disk1.img disk2.img
 
  qemu-img create -f raw disk1.img 16G
  qemu-img create -f raw disk2.img 16G
 
- time qemu-system-x86_64 \
+ qemu-system-x86_64 \
  -enable-kvm \
- -cdrom $(find ./result/iso -type f -name '*.iso') \
+ -cdrom "$(find ./result/iso -type f -name '*.iso' || true)" \
  -drive format=raw,file=disk1.img \
  -drive format=raw,file=disk2.img \
  -nographic \
