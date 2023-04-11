f7b41158268a3cf58f693d7a415a43cc47a63816
Maurice Zhou <yuchen@apvc.uk>
2023-04-11 13:16:38 +0200

    sync with openzfs-docs
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- init.sh
+++ init.sh
@@ -15,6 +15,7 @@ run_test () {
     local distro="${2}"
     sed 's|.. ifconfig:: zfs_root_test|::|g' \
 	"${path}" > "${distro}".rst
+    sed -i '/highlight:: sh/d' "${distro}".rst
 
     # Generate installation script from documentation
     python scripts/zfs_root_gen_bash.py "${distro}".rst "${distro}".sh
@@ -24,20 +25,26 @@ run_test () {
     # ensure heredocs work
     sed -i 's|^ *ZFS_ROOT_GUIDE_TEST|ZFS_ROOT_GUIDE_TEST|g' "${distro}".sh
     sed -i 's|^ *ZFS_ROOT_NESTED_CHROOT|ZFS_ROOT_NESTED_CHROOT|g' "${distro}".sh
+    sed -i 's|^ *EOF|EOF|g' "${distro}".sh
 
     # check whether nixos.sh have syntax errors
     bash -n "${distro}".sh
+
+    ## !shellcheck does not handle nested chroots
+    # create another file with <<EOF construct removed
+    sed 's|<<.*||g' "${distro}".sh > "${distro}"-shellcheck.sh
     shellcheck \
         --check-sourced \
         --enable=all \
         --shell=dash \
         --severity=style \
         --format=tty \
-        "${distro}".sh
+        "${distro}"-shellcheck.sh
 
     # Make the installation script executable and run
     chmod a+x "${distro}".sh
 }
+
 cd openzfs-docs
 run_test 'docs/Getting Started/NixOS/Root on ZFS.rst' nixos
 EOF
